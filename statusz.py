2debc5b194a3a51d65dc286a350dc4f1b31f22d4
logan114 <kepirobalazs204@gmail.com>
2023-12-13 14:49:09 +0100

    kesz szintek

--- statusz.py
+++ statusz.py
@@ -3,22 +3,7 @@ HP:int = 100
 penz:int = 0
 hatizsak = []
 igek = ["megy", "ad", "felvesz", "hasznal", "eszik", "vizsgal"]
-fonevek = ["epulet", "kut", "kastely", "penz", "vartemplom", "kamra", "ajto", "kulcs", "faajto", "etel"]
+fonevek = ["epulet", "kut", "kastely", "penz", "vartemplom", "kamra", "ajto", "kulcs", "faajto", "etel","nyugat"]
 
-""" 1 = megy
-2 = ad
-3 = felvesz
-4 = hasznal
-5 = eszik
-6 = vizsgal
 
-7 = epulet
-8 = kastely
-9 = penz
-10 = vartemplom
-11 = kamra
-12 = ajto
-13 = kulcs
-14 = faajto
-15 = etel """
 
